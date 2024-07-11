d746c30aa09c51894f15c7a2ff0059a46b075844
Bruna Garcia <47999868+GarciaBoo@users.noreply.github.com>
2024-07-11 18:09:23 -0300

    feat: add validation for active patient before scheduling appointment

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.validacoes.agendamento.ValidadorAgendamentoDeConsulta;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -19,7 +22,10 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public void agendar(DadosAgendamentoConsulta dados) {
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
+    public DadosDetalhamentoConsulta agendar(DadosAgendamentoConsulta dados) {
 
         if(!pacienteRepository.existsById(dados.idPaciente())){
             throw new ValidacaoException("Id do paciente informado não existe");
@@ -29,10 +35,14 @@ public class AgendaDeConsultas {
             throw new ValidacaoException("Id do medico informado não existe");
         }
 
+        validadores.forEach( v -> v.validar(dados));
+
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
+
+        return new DadosDetalhamentoConsulta(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
--- src/main/resources/db/migration/V8__alter-table-consultas-add-colum-motivo_cancelamento.sql
+++ src/main/resources/db/migration/V8__alter-table-consultas-add-colum-motivo_cancelamento.sql
@@ -1,1 +1,1 @@
-alter table consultas add column motivo_cancelamento varchar(100);7
+alter table consultas add column motivo_cancelamento varchar(100);
